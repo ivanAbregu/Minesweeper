26ffe506862f53d12f3f35d7fe8c05db451b50b0
ivan <ivan@bytelion.com>
2020-09-17 18:19:59 -0300

    added build files

---
+++ frontend/build/precache-manifest.df40cb53a02f3a295f639567a1bffd3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf39eb31b9737eaeb4908a768e88b6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eb0ce6a4f2435ca6329",
+    "url": "/static/css/main.b6c86bb5.chunk.css"
+  },
+  {
+    "revision": "d1aeac4e38d684a69c9e",
+    "url": "/static/js/2.bbb1254c.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.bbb1254c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb0ce6a4f2435ca6329",
+    "url": "/static/js/main.975b4365.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
