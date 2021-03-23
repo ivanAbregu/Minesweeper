adb6caeb56b26532a081a95a535ffbebc7c021b2
ivan abregu <ivan.abregu@gmail.com>
2021-03-23 16:42:28 -0300

    build front

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5cb205dcd9365b841ae1d449af85d076.js"
+  "/precache-manifest.ca81aff82c696229079449da89415ca1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
