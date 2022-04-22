72f07b9209869c94c4c39bb9de79f146b66501e4
ivan abregu <ivan.abregu@gmail.com>
2022-04-22 18:40:24 -0300

    build frontlast

---
+++ frontend/build/precache-manifest.2f56725dec99df64fb190d88c168de80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1116c3bbdeab85a666edc181258403",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98542df1aa7ad17063f4",
+    "url": "/static/css/main.b6c86bb5.chunk.css"
+  },
+  {
+    "revision": "c9354f7e5f20feb71b39",
+    "url": "/static/js/2.fa21c6f4.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.fa21c6f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98542df1aa7ad17063f4",
+    "url": "/static/js/main.96112868.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
